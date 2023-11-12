b0f9d609c1e24b121569f22b44ecb4cfe6c21f92
Ultra-tech-code <hizick27@gmail.com>
2023-11-12 13:12:53 +0100

    setup config for ottochain deloyment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,46 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+
+require('dotenv').config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    version: '0.8.17',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    //for ottochain testnet
+    'ottochain-testnet': {
+      url: 'https://gateway.testnet.octopus.network/ottochain/m4k5urt9h33dpbhgsp4lqxemo6naeihz',
+      accounts: [process.env.WALLET_KEY as string]
+    },
+    //for ottochain mainnet
+    'ottochain-mainnet': {
+      url: 'https://gateway.mainnet.octopus.network/ottochain/m4k5urt9h33dpbhgsp4lqxemo6naeihz',
+      accounts: [process.env.WALLET_KEY as string]
+    },
+  },
+  defaultNetwork: 'hardhat',
+  // etherscan: {
+  //   apiKey: {
+  //    "base-goerli": "PLACEHOLDER_STRING"
+  //   },
+  //   customChains: [
+  //     {
+  //       network: "base-goerli",
+  //       chainId: 84531,
+  //       urls: {
+  //        apiURL: "https://api-goerli.basescan.org/api",
+  //        browserURL: "https://goerli.basescan.org"
+  //       }
+  //     }
+  //   ]
+  // },
 };
 
 export default config;
